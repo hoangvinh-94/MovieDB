902a8467ccf6a9b30880c2057288be7c1adab2ed
Team_VinhPhamPro <downloadofvinh@gmail.com>
2017-05-17 16:31:18 +0700

    Add files via upload

--- MovieDB/MovieDB/ViewController.swift
+++ MovieDB/MovieDB/ViewController.swift
@@ -8,19 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout{
 
     var defaultSession: URLSession!
     var dataTask: URLSessionDataTask?
     
     @IBOutlet var collectionView: UICollectionView!
     
-    var images = [String]()
+    var posters = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        defaultSession = URLSession(configuration: URLSessionConfiguration.default, delegate: self as! URLSessionDelegate,     delegateQueue: nil)
+        self.collectionView.dataSource = self as? UICollectionViewDataSource
+        self.collectionView.delegate = self as? UICollectionViewDelegate
+        defaultSession = URLSession(configuration: URLSessionConfiguration.default, delegate: self as URLSessionDelegate,     delegateQueue: nil)
         
         requestData()
 
@@ -34,7 +36,50 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posters.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCollectionViewCell
+        
+        
+        let url = URL(string: posters[indexPath.row])
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            DispatchQueue.main.async {
+                cell.itemImageView.image = UIImage(data: data!)
+            }
+        }
+        return cell
+
+    }
+    
+    
 
 
+}
+
+extension ViewController: URLSessionDelegate, URLSessionDataDelegate {
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        do {
+            
+            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: Any]
+            //print(json["page"])
+            let movieArray = json["results"] as! [Any]
+            //            print(movieArray)
+            for index in 0...15{
+                let firstObject = movieArray[index] as! [String: Any]
+                //print(firstObject["poster_path"])
+                
+                images.append("https://image.tmdb.org/t/p/w640/" + (firstObject["poster_path"] as! String))
+            }
+            print(images)
+        } catch {
+            
+        }
+    }
+    
 }
 
